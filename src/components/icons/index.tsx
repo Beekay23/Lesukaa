903ed023f392fbd5ef89fb54db751466b3cae1f9
David Udo <65337929+davidudo@users.noreply.github.com>
2025-04-16 15:01:31 +0100

    chore: add icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,5 @@
+import { AddIcon } from "./add";
+import { MinusIcon } from "./minus";
+import { SearchIcon } from "./search";
+
+export { AddIcon, MinusIcon, SearchIcon };
---
+++ src/components/icons/minus.tsx
@@ -0,0 +1,21 @@
+import { IconProps } from "./types";
+
+export const MinusIcon: React.FC<IconProps> = ({ className }) => {
+  return (
+    <svg
+      width="44"
+      height="44"
+      viewBox="0 0 44 44"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M12.8334 22L31.1667 22"
+        stroke="white"
+        stroke-width="4"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
