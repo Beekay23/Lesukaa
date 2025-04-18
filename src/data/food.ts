6e54996a36768a99d241f50fcfe664470c00e967
David Udo <65337929+davidudo@users.noreply.github.com>
2025-04-18 10:35:07 +0100

    feat: add product images

--- src/data/food.ts
+++ src/data/food.ts
@@ -2,106 +2,342 @@ export const food = [
   {
     title: "Main Course",
     items: [
-      { name: "Egg Fried Rice", price: 2800 },
-      { name: "Tia Coconut Rice", price: 6000 },
-      { name: "Mixed Meat Fried Rice", price: 4000 },
-      { name: "Plantain", price: 1000 },
-      { name: "Coconut Rice", price: 2000 },
-      { name: "Salad", price: 500 },
-      { name: "Jollof Rice", price: 2000 },
-      { name: "Fried Rice", price: 2000 },
-      { name: "Chips", price: 2500 },
-      { name: "Native Rice", price: 2500 },
-      { name: "Asun Rice", price: 6000 },
-      { name: "Pepper Sauce", price: 700 },
-      { name: "Chinese Fried Rice", price: 3000 },
-      { name: "Plain Basmati Rice", price: 2000 },
-      { name: "Basmati Coconut Rice", price: 3800 },
-      { name: "Basmati Jollof Rice", price: 3800 },
-      { name: "Egg Sauce", price: 1500 },
-      { name: "Basmati Fried Rice", price: 3800 },
-      { name: "White Rice", price: 700 },
-      { name: "White Yam", price: 1000 },
-      { name: "Stew", price: 1000 },
-      { name: "Basmatic Asun Rice", price: 5200 },
-      { name: "Garden Egg Sauce", price: 1000 },
-      { name: "Big Tray of Asun Rice", price: 37000 },
-      { name: "Small Tray of Asun Rice", price: 18500 }
-    ]
+      {
+        name: "Egg Fried Rice",
+        price: 2800,
+        imageURL: "/food/main-course/egg-fried-rice.png",
+      },
+      {
+        name: "Tia Coconut Rice",
+        price: 6000,
+        imageURL: "/food/main-course/tia-coconut-rice.png",
+      },
+      {
+        name: "Mixed Meat Fried Rice",
+        price: 4000,
+        imageURL: "/food/main-course/mixed-meat-fried-rice.jpg",
+      },
+      {
+        name: "Plantain",
+        price: 1000,
+        imageURL: "/food/main-course/plantain.png",
+      },
+      {
+        name: "Coconut Rice",
+        price: 2000,
+        imageURL: "/food/main-course/coconut-rice.jpg",
+      },
+      { name: "Salad", price: 500, imageURL: "/food/main-course/salad.jpg" },
+      {
+        name: "Jollof Rice",
+        price: 2000,
+        imageURL: "/food/main-course/jollof-rice.jpg",
+      },
+      {
+        name: "Fried Rice",
+        price: 2000,
+        imageURL: "/food/main-course/fried-rice.jpg",
+      },
+      { name: "Chips", price: 2500, imageURL: "/food/main-course/chips.jpg" },
+      {
+        name: "Native Rice",
+        price: 2500,
+        imageURL: "/food/main-course/native-rice.png",
+      },
+      {
+        name: "Asun Rice",
+        price: 6000,
+        imageURL: "/food/main-course/asun-rice.jpg",
+      },
+      {
+        name: "Pepper Sauce",
+        price: 700,
+        imageURL: "/food/main-course/pepper-sauce.jpg",
+      },
+      {
+        name: "Chinese Fried Rice",
+        price: 3000,
+        imageURL: "/food/main-course/chinese-fried-rice.jpg",
+      },
+      {
+        name: "Plain Basmati Rice",
+        price: 2000,
+        imageURL: "/food/main-course/plain-basmati-rice.png",
+      },
+      {
+        name: "Basmati Coconut Rice",
+        price: 3800,
+        imageURL: "/food/main-course/basmati-coconut-rice.jpg",
+      },
+      {
+        name: "Basmati Jollof Rice",
+        price: 3800,
+        imageURL: "/food/main-course/basmati-jollof-rice.jpg",
+      },
+      {
+        name: "Egg Sauce",
+        price: 1500,
+        imageURL: "/food/main-course/egg-sauce.jpg",
+      },
+      {
+        name: "Basmati Fried Rice",
+        price: 3800,
+        imageURL: "/food/main-course/basmati-fried-rice.jpg",
+      },
+      {
+        name: "White Rice",
+        price: 700,
+        imageURL: "/food/main-course/white-rice.png",
+      },
+      {
+        name: "White Yam",
+        price: 1000,
+        imageURL: "/food/main-course/white-yam.png",
+      },
+      { name: "Stew", price: 1000, imageURL: "/food/main-course/stew.jpg" },
+      {
+        name: "Basmatic Asun Rice",
+        price: 5200,
+        imageURL: "/food/main-course/basmatic-asun-rice.png",
+      },
+      {
+        name: "Garden Egg Sauce",
+        price: 1000,
+        imageURL: "/food/main-course/garden-egg-sauce.jpg",
+      },
+      {
+        name: "Big Tray of Asun Rice",
+        price: 37000,
+        imageURL: "/food/main-course/big-tray-of-asun-rice.jpg",
+      },
+      {
+        name: "Small Tray of Asun Rice",
+        price: 18500,
+        imageURL: "/food/main-course/small-tray-of-asun-rice.jpg",
+      },
+    ],
   },
   {
     title: "Starters",
     items: [
-      { name: "Goat Meat Pepper Soup", price: 3000 },
-      { name: "Assorted Pepper Soup", price: 2000 },
-      { name: "Chicken Pepper Soup", price: 3000 },
-      { name: "Crocker Pepper Soup", price: 3500 },
-      { name: "Cowtail Pepper Soup", price: 2500 },
-      { name: "Pepper Goat Meat", price: 3000 },
-      { name: "Pepper Cow Tail", price: 2500 },
-      { name: "Pepper Snail", price: 3000 },
-      { name: "Pepper Chicken", price: 3000 },
-      { name: "Pepper Turkey", price: 5500 },
-      { name: "Pepper Crocker", price: 3500 },
-      { name: "Pepper Gizzard", price: 2500 },
-      { name: "Pepper Beef", price: 3000 },
-      { name: "Pepper Assorted", price: 2000 },
-      { name: "Goat Meat Peppersoup/Yam", price: 4000 }
-    ]
+      {
+        name: "Goat Meat Pepper Soup",
+        price: 3000,
+        imageURL: "/food/starters/goat-meat-pepper-soup.jpg",
+      },
+      {
+        name: "Assorted Pepper Soup",
+        price: 2000,
+        imageURL: "/food/starters/assorted-pepper-soup.jpg",
+      },
+      {
+        name: "Chicken Pepper Soup",
+        price: 3000,
+        imageURL: "/food/starters/chicken-pepper-soup.jpg",
+      },
+      {
+        name: "Crocker Pepper Soup",
+        price: 3500,
+        imageURL: "/food/starters/crocker-pepper-soup.jpg",
+      },
+      {
+        name: "Cowtail Pepper Soup",
+        price: 2500,
+        imageURL: "/food/starters/cowtail-pepper-soup.jpg",
+      },
+      {
+        name: "Pepper Goat Meat",
+        price: 3000,
+        imageURL: "/food/starters/pepper-goat-meat.jpg",
+      },
+      {
+        name: "Pepper Cow Tail",
+        price: 2500,
+        imageURL: "/food/starters/pepper-cow-tail.png",
+      },
+      {
+        name: "Pepper Snail",
+        price: 3000,
+        imageURL: "/food/starters/pepper-snail.jpg",
+      },
+      {
+        name: "Pepper Chicken",
+        price: 3000,
+        imageURL: "/food/starters/pepper-chicken.jpg",
+      },
+      {
+        name: "Pepper Turkey",
+        price: 5500,
+        imageURL: "/food/starters/pepper-turkey.jpg",
+      },
+      {
+        name: "Pepper Crocker",
+        price: 3500,
+        imageURL: "/food/starters/pepper-crocker.png",
+      },
+      {
+        name: "Pepper Gizzard",
+        price: 2500,
+        imageURL: "/food/starters/pepper-gizzard.jpg",
+      },
+      {
+        name: "Pepper Beef",
+        price: 3000,
+        imageURL: "/food/starters/pepper-beef.jpg",
+      },
+      {
+        name: "Pepper Assorted",
+        price: 2000,
+        imageURL: "/food/starters/pepper-assorted.jpg",
+      },
+      {
+        name: "Goat Meat Peppersoup/Yam",
+        price: 4000,
+        imageURL: "/food/starters/goat-meat-peppersoup-yam.jpg",
+      },
+    ],
   },
   {
     title: "Protein",
     items: [
-      { name: "Dry Fish", price: 2500 },
-      { name: "Cowtail", price: 2500 },
-      { name: "Goat Meat", price: 1500 },
-      { name: "Snail", price: 1500 },
-      { name: "Beef", price: 1500 },
-      { name: "Chicken", price: 3000 },
-      { name: "Crocker Fish", price: 3500 },
-      { name: "Turkey", price: 5500 },
-      { name: "Gizzard", price: 2500 },
-      { name: "Assorted", price: 2000 }
-    ]
+      { name: "Dry Fish", price: 2500, imageURL: "/food/protein/dry-fish.jpg" },
+      { name: "Cowtail", price: 2500, imageURL: "/food/protein/cowtail.jpg" },
+      {
+        name: "Goat Meat",
+        price: 1500,
+        imageURL: "/food/protein/goat-meat.jpg",
+      },
+      { name: "Snail", price: 1500, imageURL: "/food/protein/snail.jpg" },
+      { name: "Beef", price: 1500, imageURL: "/food/protein/beef.jpg" },
+      { name: "Chicken", price: 3000, imageURL: "/food/protein/chicken.jpg" },
+      {
+        name: "Crocker Fish",
+        price: 3500,
+        imageURL: "/food/protein/crocker-fish.jpg",
+      },
+      { name: "Turkey", price: 5500, imageURL: "/food/protein/turkey.jpg" },
+      { name: "Gizzard", price: 2500, imageURL: "/food/protein/gizzard.jpg" },
+      { name: "Assorted", price: 2000, imageURL: "/food/protein/assorted.jpg" },
+    ],
   },
   {
     title: "National",
     items: [
-      { name: "Native Soup", price: 2000 },
-      { name: "Afang Soup", price: 1800 },
-      { name: "Egusi Soup", price: 1800 },
-      { name: "Okro Soup", price: 1800 },
-      { name: "Bitterleaf Soup", price: 1600 },
-      { name: "Oha Soup", price: 1600 },
-      { name: "Banga Stew", price: 1000 },
-      { name: "Ukwa", price: 2500 }
-    ]
+      {
+        name: "Native Soup",
+        price: 2000,
+        imageURL: "/food/national/native-soup.jpg",
+      },
+      {
+        name: "Afang Soup",
+        price: 1800,
+        imageURL: "/food/national/afang-soup.jpg",
+      },
+      {
+        name: "Egusi Soup",
+        price: 1800,
+        imageURL: "/food/national/egusi-soup.jpg",
+      },
+      {
+        name: "Okro Soup",
+        price: 1800,
+        imageURL: "/food/national/okro-soup.jpg",
+      },
+      {
+        name: "Bitterleaf Soup",
+        price: 1600,
+        imageURL: "/food/national/bitterleaf-soup.jpg",
+      },
+      {
+        name: "Oha Soup",
+        price: 1600,
+        imageURL: "/food/national/oha-soup.jpg",
+      },
+      {
+        name: "Banga Stew",
+        price: 1000,
+        imageURL: "/food/national/banga-stew.jpg",
+      },
+      { name: "Ukwa", price: 2500, imageURL: "/food/national/ukwa.jpg" },
+    ],
   },
   {
     title: "Swallow",
     items: [
-      { name: "Garri", price: 700 },
-      { name: "Pounded Yam", price: 1800 },
-      { name: "Oat Meal", price: 1800 },
-      { name: "Semo", price: 1000 }
-    ]
+      { name: "Garri", price: 700, imageURL: "/food/swallow/garri.jpg" },
+      {
+        name: "Pounded Yam",
+        price: 1800,
+        imageURL: "/food/swallow/pounded-yam.jpg",
+      },
+      { name: "Oat Meal", price: 1800, imageURL: "/food/swallow/oat-meal.jpg" },
+      { name: "Semo", price: 1000, imageURL: "/food/swallow/semo.jpg" },
+    ],
   },
   {
     title: "African Dish",
     items: [
-      { name: "Abacha/Fish/Canda", price: 3400 },
-      { name: "Tray of Abacha", price: 37000 },
-      { name: "Half Tray of Abacha", price: 19000 },
-      { name: "Abacha with Fish", price: 3000 },
-      { name: "Abacha with Canda", price: 1900 },
-      { name: "Only Abacha", price: 1500 },
-      { name: "Ugba and Fish", price: 2500 },
-      { name: "Only Ugba", price: 1000 },
-      { name: "Extra Fish", price: 1500 },
-      { name: "Canda", price: 400 },
-      { name: "Nkwobi", price: 6000 },
-      { name: "Isi-Ewu", price: 8000 },
-      { name: "Chicken Nkwobi", price: 12500 }
-    ]
-  }
+      {
+        name: "Abacha/Fish/Canda",
+        price: 3400,
+        imageURL: "/food/african-dish/abacha-fish-canda.jpg",
+      },
+      {
+        name: "Tray of Abacha",
+        price: 37000,
+        imageURL: "/food/african-dish/tray-of-abacha.jpg",
+      },
+      {
+        name: "Half Tray of Abacha",
+        price: 19000,
+        imageURL: "/food/african-dish/half-tray-of-abacha.jpg",
+      },
+      {
+        name: "Abacha with Fish",
+        price: 3000,
+        imageURL: "/food/african-dish/abacha-with-fish.jpg",
+      },
+      {
+        name: "Abacha with Canda",
+        price: 1900,
+        imageURL: "/food/african-dish/abacha-with-canda.jpg",
+      },
+      {
+        name: "Only Abacha",
+        price: 1500,
+        imageURL: "/food/african-dish/only-abacha.jpg",
+      },
+      {
+        name: "Ugba and Fish",
+        price: 2500,
+        imageURL: "/food/african-dish/ugba-and-fish.jpg",
+      },
+      {
+        name: "Only Ugba",
+        price: 1000,
+        imageURL: "/food/african-dish/only-ugba.jpg",
+      },
+      {
+        name: "Extra Fish",
+        price: 1500,
+        imageURL: "/food/african-dish/extra-fish.jpg",
+      },
+      { name: "Canda", price: 400, imageURL: "/food/african-dish/canda.jpg" },
+      {
+        name: "Nkwobi",
+        price: 6000,
+        imageURL: "/food/african-dish/nkwobi.jpg",
+      },
+      {
+        name: "Isi-Ewu",
+        price: 8000,
+        imageURL: "/food/african-dish/isi-ewu.jpg",
+      },
+      {
+        name: "Chicken Nkwobi",
+        price: 12500,
+        imageURL: "/food/african-dish/chicken-nkwobi.jpg",
+      },
+    ],
+  },
 ];
