6171d00a39d16ab6a1c5db49ceb809f66e1b9875
Beekay23 <kingblessing567@gmail.com>
2025-05-12 07:42:40 +0100

    Update food.ts

--- src/data/food.ts
+++ src/data/food.ts
@@ -4,7 +4,7 @@ export const food = [
     items: [
       {
         name: "Egg Fried Rice",
-        price: 2800,
+        price: 3000,
         imageURL: "/food/main-course/egg-fried-rice.png",
       },
       {
